d6e04ddb6b917e395eec7673b7802eed3425f407
kimdhamilton <kimdhamilton@gmail.com>
2019-02-02 12:40:38 -0800

    add create-repo scripts

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# About
+## REPO_NAME
 
-Create repo tools
+We encourage contributions meeting the [Contribution
+Guidelines](CONTRIBUTING.md).  While we prefer the creation of issues
+and Pull Requests in the GitHub repository, discussions often occur
+on the
+[public-credentials](http://lists.w3.org/Archives/Public/public-credentials/)
+mailing list as well.
---
+++ create.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Run from this script's parent dir
+# 
+SCRIPT_HOME=../create-repo/
+
+REPO_NAME=$1;shift
+OWNERS="@$1";shift
+for arg in "$@"
+do
+    OWNERS="$OWNERS @$arg"
+done
+
+echo "Repo name: ${REPO_NAME}"
+echo "Owners: ${OWNERS}"
+
+git clone https://github.com/w3c-ccg/$REPO_NAME.git
+cd $REPO_NAME
+
+# Create gh-pages branch
+git checkout --orphan gh-pages
+git rm -rf .
+# Add default files
+cp $SCRIPT_HOME/CODEOWNERS .
+cp $SCRIPT_HOME/CONTRIBUTING.md .
+cp $SCRIPT_HOME/LICENSE.md .
+cp $SCRIPT_HOME/README.md .
+
+sed -i '' -e "s/THE_OWNERS/${OWNERS}/g" CODEOWNERS
+sed -i '' -e "s/REPO_NAME/${REPO_NAME}/g" README.md
+
+echo "My Page" > index.html
+git add index.html
+
+# Make sure we want to continue
+read -r -p "Check for problems, press 'y' when ready to proceed: " PROCEED
+
+echo "Proceed: $PROCEED"
+
+if [ "$PROCEED" != "y" ]
+then
+   echo "Aborting git commit."
+   exit
+fi
+
+git commit -a -m "First pages commit"
+git push --set-upstream origin gh-pages
+
+
